3c2c42843b855a712b622a205aae82be70f99ebe
davidsantana06 <davidsantana06@hotmail.com>
2024-06-04 10:38:04 -0300

    feat: add extensions

---
+++ app/extensions.py
@@ -0,0 +1,12 @@
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
+from flask_sqlalchemy import SQLAlchemy
+from flask_socketio import SocketIO
+from flask_wtf import CSRFProtect
+
+
+database = SQLAlchemy()
+bcrypt = Bcrypt()
+csrf = CSRFProtect()
+login_manager = LoginManager()
+socket_io = SocketIO()
