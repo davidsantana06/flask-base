b618e25e4cc88a648e01dc0309341a92936c6743
davidsantana06 <davidsantana06@hotmail.com>
2024-06-04 10:40:20 -0300

    feat: add utils lib

---
+++ app/lib/utils/url_composer.py
@@ -0,0 +1,45 @@
+from flask import url_for
+
+
+_CSS_EXTENSION = '.css'
+_CSS_FILE_PATH = 'css/{}'
+_JS_EXTENSION = '.js'
+_JS_FILE_PATH = 'js/{}'
+_IMG_FILE_PATH = 'img/{}'
+_OWN_MODULE = '.'
+
+
+def _url_for_static(file_name: str, module: str) -> str:
+    return url_for(f'{module}static', filename=file_name)
+
+
+def url_for_css(css_name: str, module: str = _OWN_MODULE) -> str:
+    if not css_name.endswith(_CSS_EXTENSION):
+        css_name += _CSS_EXTENSION
+    return _url_for_static(_CSS_FILE_PATH.format(css_name), module)
+
+
+def url_for_common_css(css_name: str) -> str:
+    if not css_name.endswith(_CSS_EXTENSION):
+        css_name += _CSS_EXTENSION
+    return _url_for_static(_CSS_FILE_PATH.format(css_name), '')
+
+
+def url_for_img(img_name: str, module: str = _OWN_MODULE) -> str:
+    return _url_for_static(_IMG_FILE_PATH.format(img_name), module)
+
+
+def url_for_common_img(img_name: str) -> str:
+    return _url_for_static(_IMG_FILE_PATH.format(img_name), '')
+
+
+def url_for_js(js_name: str, module: str = _OWN_MODULE) -> str:
+    if not js_name.endswith(_JS_EXTENSION):
+        js_name += _JS_EXTENSION
+    return _url_for_static(_JS_FILE_PATH.format(js_name), module)
+
+
+def url_for_common_js(js_name: str) -> str:
+    if not js_name.endswith(_JS_EXTENSION):
+        js_name += _JS_EXTENSION
+    return _url_for_static(_JS_FILE_PATH.format(js_name), '')
